d7b5638117b5478259c612234a429f6e166e540b
= <=>
2021-01-10 19:17:59 +0300

    firebase updated

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,13 +7,14 @@ import { useStateValue } from './StateProvider'
 
 const Checkout = () => {
 
-    const [ {basket}, dispatch] = useStateValue();
+    const [ {basket , user }, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
             <div className="checkout-left">
                 <img src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' className="checkout-ad" />
                 <div>
+                    <h3>Hello {user?.email}</h3>
                     <h2 className="checkout-title">Your Shopping Basket</h2>
                     {basket.map(item => ( 
                         <CheckoutProduct
